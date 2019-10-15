aad73975ced5c3caa0768ff73f2b6f1666b0d70c
sbijukchhe <skbijukchhe@gmail.com>
2019-10-15 16:19:26 -0400

    Using Database - Based Authentication

---
+++ src/main/java/com/example/demo/SpringBoot404DbBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot404DbBasedAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot404DbBasedAuthenticationApplication.class, args);
+    }
+
+}
